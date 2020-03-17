d4faec4ef75b6cff16b648932287060065c0ed3f
Tim Booth <tim.booth@ed.ac.uk>
2020-03-17 14:40:52 +0000

    I'm now happy with the adding of the FastQC report links. This should fix the problem reported by Javier.

--- multiqc_edgen/modules/edgen_fastqc_original/edgen_fastqc_original.py
+++ multiqc_edgen/modules/edgen_fastqc_original/edgen_fastqc_original.py
@@ -6,7 +6,7 @@
 import logging
 import sys, os, re
 import shutil
-from collections import namedtuple
+from collections import namedtuple, OrderedDict
 from distutils.version import StrictVersion
 
 from multiqc import config
@@ -29,29 +29,27 @@ class MultiqcModule(BaseMultiqcModule):
         # Initialise the parent object
         super(MultiqcModule, self).__init__(name='FastQC Original Reports', anchor='edgen_fastqc_original',
             href='http://www.bioinformatics.babraham.ac.uk/projects/fastqc/',
-            info="are the original HTML files produced by FastQC.")
+            info="are the original HTML files produced by FastQC. Paired-end runs have two reports per library.")
 
-        # Find any HTML reports. We have to do this by finding the zips. Note there are maybe two FASTQC reports
+        # Find any HTML reports. We have to do this by finding the associated zips! Note there are maybe two FASTQC reports
         # per fragment, so each report looks like:
-        FQCReport = namedtuple('FQCReport', "sample label read file".split())
+        FQCReport = namedtuple('FQCReport', "sample read file".split())
         self.html_reports = []
-
-        FIXME FIXME
 
         for zip_report in self.find_log_files('fastqc/zip', filehandles=True):
 
-            rep_name = ( "{}_{}".format(zip_report['s_name'], zip_report.get('read_pairs'))
-                          if zip_report.get('read_pairs')
-                          else zip_report['s_name'] )
+            self.html_reports.append( FQCReport( zip_report['s_name'],
+                                                 zip_report.get('read_pairs'),
+                                                 re.sub('\.zip$', '.html', zip_report['f'].name) ) )
 
-            self.html_reports[rep_name] = re.sub('\.zip$', '.html', zip_report['f'].name)
             zip_report['f'].close()
 
         if not self.html_reports:
             log.debug("Could not find any reports in {}".format(config.analysis_dir))
             raise UserWarning
 
-        log.info("Found {} reports".format(len(self.html_reports)))
+        self.samples_list = sorted(set(r.sample for r in self.html_reports))
+        log.info("Found {} reports for {} samples".format(len(self.html_reports), len(self.samples_list)))
 
         self.add_section( name = "Reports",
                           content = self.tack_on_reports() )
@@ -59,19 +57,29 @@ class MultiqcModule(BaseMultiqcModule):
     def tack_on_reports(self):
         """ Copy every file into the data_dir and bung in a link to it here.
         """
-        links = dict()
+        links = OrderedDict()
 
-        for s_name, flist in self.html_reports.items():
+        # Go through the already-sorted list of samples.
+        for s_name in self.samples_list:
             links[s_name] = "<span class='alt_col_link'>"
 
-            for f in flist:
-                fname = os.path.basename(f)
+            # Cycle through the reports. Presumably there are one or two.
+            reps = sorted( [ r for r in self.html_reports if r.sample == s_name ],
+                           key = lambda r: r.read )
+            for n, rep in enumerate(reps):
 
-                shutil.copy(f, os.path.join( config.data_dir, fname ))
+                # This is a little funky but seems most legible...
+                rep_label = "{}_{}".format(rep.sample, rep.read) if len(reps) > 1 else rep.sample
+                link_label = rep_label if n == 0 else "..._{}".format(rep.read)
+                fname = os.path.basename(rep.file)
+
+                shutil.copy(rep.file, os.path.join( config.data_dir, fname ))
                 file_relpath = os.path.join( config.data_dir_name, fname )
 
-                links[s_name] += "<a href='{l}' title='{t} FastQC Report'>{t}</a>".format(
-                                           l=url_escape(file_relpath), t=html_escape(s_name) )
+                links[s_name] += "<a href='{f}' title='{rl} FastQC Report'>{ll}</a> ".format(
+                                        f = url_escape(file_relpath),
+                                        rl = html_escape(rep_label),
+                                        ll = html_escape(link_label) )
 
             links[s_name] += "</span>"
 
